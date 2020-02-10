0e175bd3510dbab4c3170c74f6447505ed32c04f
Jake Ash and Trevor Mackin <jake@>
2020-02-10 08:53:05 -0800

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Help Queue</h1>
+    <h3>3a</h3>
+    <h3>Thato and Haley</h3>
+    <p><em>Firebase will not save record!</em></p>
+  </div>,
+  document.getElementById('react-app-root')
+);
