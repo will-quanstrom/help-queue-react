b78cde33ca1b26f714c5d3c3f7bfe345cd6d1b6b
Student <example@example.com>
2020-02-18 08:57:56 -0800

    add linter rules associated with states

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -14,6 +14,7 @@ class NewTicketControl extends React.Component {
   }
 
   handleTroubleshootingConfirmation(){
+    this.state = {test: 'hey'};
     this.setState({formVisibleOnPage: true});
   }
 
